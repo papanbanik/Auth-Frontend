a35ce9fccbbe8c598675a81f428c95065617e6c6
Papan Banik <papanbanik58@gmail.com>
2026-06-03 17:49:41 +0600

    solve google auth login

--- Frontend/app/reset-password/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import React, { useState } from 'react'
-const Page = () => {
-  const [state,setState]= useState('Sign up')
-  return (
-    <div>ThisPage Coming soon</div>
-  )
-}
-export default Page
